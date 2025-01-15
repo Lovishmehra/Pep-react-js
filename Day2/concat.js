d6d6682af83bcdd3bf090ee499cdde034c6433df
Lovish Mehra <118118547+Lovishmehra@users.noreply.github.com>
2025-01-15 15:46:29 +0530

    Add files via upload

---
+++ Day2/concat.js
@@ -0,0 +1,8 @@
+let universityname = "LPU";
+console.log("Welcome to "+ universityname +" University");
+
+let state = "Punjab"
+console.log(`Welcome to ${state}`);
+
+let name = "Lovish";
+console.log(`My name is ${name}`);
